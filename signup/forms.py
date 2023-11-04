39d87f0c87c714095ec44f2d2751fdc88d5f7089
Jaimilit <>
2023-11-04 10:12:22 +0000

    start post detail for user form

--- signup/forms.py
+++ signup/forms.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email',)
         labels = {
             'username': 'Username (cannot be changed)',
         }
